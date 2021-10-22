954764408b65cf8a5e421405ac0137338b9c745c
kaori1970 <1970593h@stu.kobe-u.ac.jp>
2021-10-22 23:03:09 +0900

    change 2-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -5,6 +5,22 @@ function setup() {
   noStroke();
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
+      if( (i + j) % 2 == 1){
+        fill("gray");
+      }else{
+        fill(255,255,255);
+      }
+      rect(size*i,size*j,25,25);
+
+      if((i + j) % 2 == 1){
+      if(j<3){
+        fill(255,0,0);
+        ellipse(size*i+(25/2),size*j+(25/2),23);
+      }else if (j>4) {
+        fill("#000000");
+        ellipse(size*i+(25/2),size*j+(25/2),23);
+      }
+    }
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
     }
   }
