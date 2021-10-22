1cf95d274d9f79fc0cc4f41fae81df083010047d
kaori1970 <1970593h@stu.kobe-u.ac.jp>
2021-10-23 00:10:38 +0900

    change 2-q3
    大変でした

--- section2-q3/sketch.js
+++ section2-q3/sketch.js
@@ -14,22 +14,57 @@ function setup() {
   const cy = height / 2;
   const maxR = min(width, height); // 大きさは幅と高さのうち小さい方
 
-  drawCircle(black, maxR);
-  drawArcs(green, red, maxR * 0.8);
-  // BLANK[1] (hint: drawArcs x 3, drawCircle x 1)
-  drawCircle(red, maxR * 0.05);
+  drawCircle(cx, cy, black, maxR);　//一番外の円
+  drawArcs(cx, cy, green, red, maxR*0.8) //一番外側の赤緑
+  drawArcs(cx, cy, cream, black, maxR*0.75)
+  drawArcs(cx, cy, green, red, maxR*0.5)
+  drawArcs(cx, cy, cream, black, maxR*0.45)
+  drawCircle(cx, cy, green, maxR*0.1);　//二番目の円
+  drawCircle(cx, cy, red, maxR * 0.05);　//三番目の円
+  number(20,192,28)
+  number(1,250,39)
+  number(18,297,68)
+  number(4,340,109)
+  number(13,360,160)
+  number(6,373,211)
+  number(10,360,265)
+  number(15,335,311)
+  number(2,297,357)
+  number(17,240,380)
+  number(3,190,385)
+  number(19,129,375)
+  number(7,90,351)
+  number(16,40,311)
+  number(8,25,260)
+  number(11,9,205)
+  number(14,20,153)
+  number(9,49,103)
+  number(12,85,60)
+  number(5,143,37)
+  number
 }
 
-function drawCircle(c, r){
+function drawCircle(x, y, c, r){
   fill(c);
-  ellipse(cx, cy, r, r);
+  ellipse(x, y, r, r);
 }
 
-function drawArcs(c1, c2, r) {
+function drawArcs(x, y, c1, c2, r) {
   for (let i = 0; i < 20; i++) {
-    let start = TWO_PI / 20 * i;
-    let stop = TWO_PI / 20 * (i + 1);
-    fill(i % 2 == 0 ? c1 : c2);
-    arc(cx, cy, r, r, start, stop, PIE);
+    let start = (TWO_PI / 20 * i)-PI*(1/(7.));
+    let stop = TWO_PI / 20 * (i + 1)-PI*(1/(7));
+    if(i % 2 == 1){
+      fill(c1);
+    }else{
+      fill(c2);
   }
+    arc(x, y, r, r, start, stop, PIE);
+  }
+}
+
+function number(a,x,y){
+  fill(255);
+  textSize(23);
+  textFont("Times New Roman")
+  text(a,x,y)
 }
