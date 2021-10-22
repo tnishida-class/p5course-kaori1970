2d1c3353780fa330e601043b83593a8d197901cf
kaori1970 <1970593h@stu.kobe-u.ac.jp>
2021-10-22 22:19:30 +0900

    change 2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,14 @@
 // 小手調べ
+
 function setup(){
-  for(let i = 0; i < 10; i++){
-    // BLANK[1]
+  noFill();
+  createCanvas(200,200);
+  for(let i = 1; i < 11; i++){
+    if(i<=5){
+      stroke(0, 0, 255);
+    }else{
+      stroke(255, 0, 0);
+    }
+    ellipse(100,100,i*10);
   }
 }
